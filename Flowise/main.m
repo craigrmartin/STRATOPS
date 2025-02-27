4cb8529e0c6cdf2ab6d8cc24a8d7619f9b76133a
craigrmartin <craig@designchain.co>
2025-02-27 11:43:42 +0000

    Initial Commit

---
+++ Flowise/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Flowise
+//
+//  Created by Craig Martin on 27/02/2025.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
